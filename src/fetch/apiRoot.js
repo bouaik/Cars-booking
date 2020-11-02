3f09f68672c852e18795178d14a9cb0c97cfb5e5
Lhoussaine <bouaik.lhou@gmail.com>
2020-11-02 12:00:46 +0100

    Add heroku link to apiRoot file

--- src/fetch/apiRoot.js
+++ src/fetch/apiRoot.js
@@ -1,4 +1,4 @@
 const DEV_URL = 'http://localhost:4000';
-const PROD_URL = 'https://[APP-NAME].herokuapp.com';
+const PROD_URL = 'https://cars-booking-api.herokuapp.com';
 export const API_ROOT =
   process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
