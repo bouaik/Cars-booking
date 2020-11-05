d91c0322172bade97983ec362d743faee3703976
Lhoussaine <bouaik.lhou@gmail.com>
2020-11-05 12:52:01 +0100

    Display Appointements

--- src/components/Appointements.js
+++ src/components/Appointements.js
@@ -2,12 +2,27 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchAppointements } from '../redux/actions/appointementActions';
 
-const Appointements = ({ fetchAppointements, userReducer }) => {
+const Appointements = ({
+  fetchAppointements,
+  userReducer,
+  appointementReducer,
+}) => {
   const username = userReducer.user.user.username;
   useEffect(() => {
     fetchAppointements(username);
   }, [fetchAppointements, username]);
-  return <div>This is Appointements page</div>;
+  return (
+    <div>
+      My Appointements
+      {appointementReducer.appointements.map((appointement) => (
+        <div key={appointement.id}>
+          <h3>{appointement.city}</h3>
+          <h4>{appointement.date}</h4>
+          <h4>{appointement.time}</h4>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
